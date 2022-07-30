dd7224b049f7812c190f2ee39e6e21ec0d2c2d63
as <allensnape@qq.com>
2022-07-30 18:01:31 +0800

    refactor: refine

--- src/hook/useStoredState.ts
+++ src/hook/useStoredState.ts
@@ -1,15 +1,18 @@
 import {Dispatch, SetStateAction, useCallback, useState} from 'react'
 
-function jsonSafeParse<T>(value: string): T | undefined {
+function jsonSafeParse<T>(value?: string | null): T | undefined {
   try {
-    return JSON.parse(value)
+    if (value !== undefined && value !== null) {
+      return JSON.parse(value)
+    }
   } catch {
-    return undefined
+    // nothing here
   }
+  return undefined
 }
 
 export default function useStoredState<T>(key: string, defaultValue?: T, storage: Storage = window.localStorage): [T, Dispatch<SetStateAction<T>>] {
-  const [state, setState] = useState<T>(() => jsonSafeParse<T>(key) || defaultValue as T)
+  const [state, setState] = useState<T>(() => jsonSafeParse<T>(storage.getItem(key)) || defaultValue as T)
   const setStateProxy = useCallback((value: T | ((old: T) => T)) => {
     setState(old => {
       const v = value instanceof Function ? value(old) : value
